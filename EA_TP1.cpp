d74361a8ab1afb084e3ae8cce2da6ac54ea67a0b
Tiago Oliveira <tiago.o.tocha@gmail.com>
2022-03-16 17:00:09 +0000

    175 no mooshak

--- EA_TP1.cpp
+++ EA_TP1.cpp
@@ -6,14 +6,17 @@
 
 using namespace std;
 
-class Piece {
+class Piece
+{
 public:
     vector<int> num;
     int row;
     int col;
     bool used;
+    bool tested;
 
-    Piece(int n0, int n1, int n2, int n3) {
+    Piece(int n0, int n1, int n2, int n3)
+    {
         num.push_back(n0);
         num.push_back(n1);
         num.push_back(n2);
@@ -22,9 +25,11 @@ public:
         row = 0;
         col = 0;
         used = false;
+        tested = false;
     }
 
-    void Rotate90() {
+    void Rotate90()
+    {
         int aux = num[1];
         num[1] = num[0];
         num[0] = num[3];
@@ -32,7 +37,8 @@ public:
         num[2] = aux;
     }
 
-    void Rotate180() {
+    void Rotate180()
+    {
         int aux = num[2];
         num[2] = num[0];
         num[0] = aux;
@@ -41,7 +47,8 @@ public:
         num[1] = aux;
     }
 
-    void Rotate270() {
+    void Rotate270()
+    {
         int aux = num[3];
         num[3] = num[0];
         num[0] = num[1];
@@ -49,21 +56,19 @@ public:
         num[2] = aux;
     }
 
-    pair<int, int> getBellowAbove() {
+    pair<int, int> getBellowAbove()
+    {
         return {num[2], num[1]};
     }
 
-    pair<int, int> getLeftRight() {
+    pair<int, int> getLeftRight()
+    {
         return {num[3], num[2]};
     }
-
-    bool operator==(const Piece &rhs) const {
-        return row == rhs.row && col == rhs.col && used == rhs.used && num == rhs.num;
-    }
-
 };
 
-class Board {
+class Board
+{
 public:
     int n_pieces;
     int rows;
@@ -71,7 +76,8 @@ public:
     vector<Piece *> pieces; // um vetor funciona como um Arraylist em Java, nao precisa de ter tamanho definido
     vector<Piece *> board;
 
-    Board(int np, int r, int c) {
+    Board(int np, int r, int c)
+    {
         n_pieces = np;
         rows = r;
         columns = c;
@@ -80,24 +86,31 @@ public:
         // com o tamanho final do mesmo e o numero existente de pecas
     }
 
-    void removeLeftPiece() {
+    void removeLeftPiece()
+    {
         board.back()->used = false;
         board.back()->col = board.back()->row = 0;
         board.pop_back();
     }
 
-    void removeToALimit(int index) {
-        for (int i = board.size() - 1; i >= index; i--) {
+    void removeToALimit(int index)
+    {
+        for (int i = board.size() - 1; i >= index; i--)
+        {
             board[i]->used = false;
             board[i]->col = board[i]->row = 0;
             board.pop_back();
         }
     }
 
-    void printBoard() {
-        for (int row = 1; row <= this->rows; row++) {
-            for (Piece *piece: this->board) {
-                if (piece->row == row) {
+    void printBoard()
+    {
+        for (int row = 1; row <= this->rows; row++)
+        {
+            for (Piece *piece : this->board)
+            {
+                if (piece->row == row)
+                {
                     cout << piece->num[0] << " " << piece->num[1];
                     if (piece->col != columns)
                         cout << "  ";
@@ -105,8 +118,10 @@ public:
                         cout << endl;
                 }
             }
-            for (Piece *piece: this->board) {
-                if (piece->row == row) {
+            for (Piece *piece : this->board)
+            {
+                if (piece->row == row)
+                {
                     cout << piece->num[3] << " " << piece->num[2];
                     if (piece->col != columns)
                         cout << "  ";
@@ -120,7 +135,7 @@ public:
     }
 };
 
-map <pair<int, int>, vector<Piece *>> pairs; // equivalente a dicionarios em python
+map<pair<int, int>, vector<Piece *>> pairs; // equivalente a dicionarios em python
 map<int, int> occurrences;
 
 void getPairsAndOccurrencies(Piece *p);
@@ -133,14 +148,16 @@ bool addPrimeiraColuna(Board *b, int row, int col, pair<int, int> p);
 
 bool addPeca(Board *b, int row, int col, vector<Piece *> intersection, vector<int> trio);
 
-vector<Piece *> Intersection(vector<Piece *> &nums1, vector<Piece *> &nums2);
+vector<Piece *> Intersection(vector<Piece *> &nums1, vector<int> trio);
 
 void reset(Board *b);
 
-void getPairsAndOccurrencies(Piece *p) {
+void getPairsAndOccurrencies(Piece *p)
+{
     // percorre a peca e conta as occorencias de cada numero
     // cria ainda os 4 pares possiveis na peca
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++)
+    {
         if (occurrences.find(p->num[i]) == occurrences.end())
             occurrences.insert({p->num[i], 1});
         else
@@ -155,7 +172,8 @@ void getPairsAndOccurrencies(Piece *p) {
     }
 }
 
-bool tree(Board *b, int row, int col) {
+bool tree(Board *b, int row, int col)
+{
     Piece *piece = NULL;
     Piece *piece2 = NULL;
 
@@ -163,7 +181,8 @@ bool tree(Board *b, int row, int col) {
         return false; // verifica se o ponteiro para a Board existe ou nao e verifica se o vetor de pecas esta vazio
 
     // peca esta na primeira linha
-    if (row == 1 && col <= b->columns) {
+    if (row == 1 && col <= b->columns)
+    {
         piece = b->board[col - 2];
 
         // nao foi encontrada nenhuma peca com a coluna e a linha pretendida
@@ -175,10 +194,11 @@ bool tree(Board *b, int row, int col) {
         if (pairs.find(p) == pairs.end())
             return false;
 
-        if (addPrimeiraLinha(b, 1, col, p)) {
+        if (addPrimeiraLinha(b, 1, col, p))
             return true;
-        }
-    } else if (row > 1 && row <= b->rows && col == 1) {
+    }
+    else if (row > 1 && row <= b->rows && col == 1)
+    {
         // get peca de cima
         piece2 = b->board[(row - 2) * (b->columns)];
 
@@ -192,12 +212,14 @@ bool tree(Board *b, int row, int col) {
         if (pairs.find(p2) == pairs.end())
             return false;
 
-        if (addPrimeiraColuna(b, row, 1, p2)) { // nao esta a aplicar a rotacao correta
+        if (addPrimeiraColuna(b, row, 1, p2)) // nao esta a aplicar a rotacao correta
             return true;
-        }
-    } else if (row > 1 && row <= b->rows && col > 1 && col <= b->columns) {
+
+    }
+    else if (row > 1 && row <= b->rows && col > 1 && col <= b->columns)
+    {
         // get peca de cima e da esquerda
-        piece = b->board[(row - 1) * b->columns + col - 2]; // peca da esquerda
+        piece = b->board[(row - 1) * b->columns + col - 2];      // peca da esquerda
         piece2 = b->board[(row - 2) * (b->columns) + (col - 1)]; // peca de cima
 
         // nao foi encontrada nenhuma peca com a coluna e a linha pretendida
@@ -216,35 +238,45 @@ bool tree(Board *b, int row, int col) {
             return false;
 
         // vector com pecas que encaixam segundo o par da peca da esquerda
-        vector < Piece * > possibleLeft = pairs[p];
-
-        // vector com pecas que encaixam segundo o par da peca de cima
-        vector < Piece * > possibleBellow = pairs[p2];
-
-        // intersecao dos 2 vectors
-        vector < Piece * > intersection = Intersection(possibleBellow, possibleLeft);
-
-        if (intersection.empty())
-            return false;
+        vector<Piece *> possibleLeft = pairs[p];
 
         // {bellow, above=left, right}
         vector<int> trio = {p.first, p.second, p2.second};
 
-        if (addPeca(b, row, col, intersection, trio)) { // nao esta a aplicar a rotacao correta
-            return true;
+        // intersecao dos 2 vectors
+        vector<Piece *> intersection = Intersection(possibleLeft, trio);
+
+        if (intersection.empty())
+            return false;
+
+        /*for (auto &i : intersection)
+        {
+            cout << i->num[0] << " " << i->num[1] << " " << i->num[2] << " " << i->num[3] << endl;
         }
+        cout << endl;*/
+
+        if (addPeca(b, row, col, intersection, trio)) // nao esta a aplicar a rotacao correta
+            return true;
+
     }
     return false;
 }
 
-bool addPrimeiraLinha(Board *b, int row, int col, pair<int, int> p) {
-    for (auto &match: pairs[p]) {
-        if (!match->used) {
-            if (match->num[3] == p.second && match->num[2] == p.first) match->Rotate90();
+bool addPrimeiraLinha(Board *b, int row, int col, pair<int, int> p)
+{
+    for (auto &match : pairs[p])
+    {
+        if (!match->used)
+        {
 
-            if (match->num[2] == p.second && match->num[1] == p.first) match->Rotate180();
+            if (match->num[3] == p.second && match->num[2] == p.first)
+                match->Rotate90();
 
-            if (match->num[1] == p.second && match->num[0] == p.first) match->Rotate270();
+            if (match->num[2] == p.second && match->num[1] == p.first)
+                match->Rotate180();
+
+            if (match->num[1] == p.second && match->num[0] == p.first)
+                match->Rotate270();
 
             match->col = col;
             match->row = row;
@@ -259,20 +291,21 @@ bool addPrimeiraLinha(Board *b, int row, int col, pair<int, int> p) {
             {
                 row++;
                 col = 1;
-            } else
+            }
+            else
                 col++;
 
             if (tree(b, row, col))
                 return true;
 
-            if (col == 1) { // se estiver no inicio da linha volta para o final da linha anterior
+            if (col == 1)
+            { // se estiver no inicio da linha volta para o final da linha anterior
                 row--;
                 col = b->columns;
-            } else
+            }
+            else
                 col--;
 
-            match->row = match->col = 0;
-            match->used = false;
             int pieceIndex = find(b->board.begin(), b->board.end(), match) - b->board.begin();
             b->removeToALimit(pieceIndex);
         }
@@ -281,15 +314,21 @@ bool addPrimeiraLinha(Board *b, int row, int col, pair<int, int> p) {
     return false;
 }
 
-bool addPrimeiraColuna(Board *b, int row, int col, pair<int, int> p) {
-    for (auto &match: pairs[p]) {
-        if (!match->used) {
+bool addPrimeiraColuna(Board *b, int row, int col, pair<int, int> p)
+{
+    for (auto &match : pairs[p])
+    {
+        if (!match->used)
+        {
 
-            if (match->num[0] == p.second && match->num[3] == p.first) match->Rotate90();
+            if (match->num[0] == p.second && match->num[3] == p.first)
+                match->Rotate90();
 
-            if (match->num[3] == p.second && match->num[2] == p.first) match->Rotate180();
+            if (match->num[3] == p.second && match->num[2] == p.first)
+                match->Rotate180();
 
-            if (match->num[2] == p.second && match->num[1] == p.first) match->Rotate270();
+            if (match->num[2] == p.second && match->num[1] == p.first)
+                match->Rotate270();
 
             match->col = col;
             match->row = row;
@@ -302,7 +341,8 @@ bool addPrimeiraColuna(Board *b, int row, int col, pair<int, int> p) {
 
             if (b->columns == 1)
                 row++;
-            else {
+            else
+            {
                 col++;
             }
 
@@ -311,10 +351,9 @@ bool addPrimeiraColuna(Board *b, int row, int col, pair<int, int> p) {
 
             if (b->columns == 1)
                 row--;
-            else col--;
+            else
+                col--;
 
-            match->row = match->col = 0;
-            match->used = false;
             int pieceIndex = find(b->board.begin(), b->board.end(), match) - b->board.begin();
             b->removeToALimit(pieceIndex);
         }
@@ -323,15 +362,21 @@ bool addPrimeiraColuna(Board *b, int row, int col, pair<int, int> p) {
     return false;
 }
 
-bool addPeca(Board *b, int row, int col, vector<Piece *> intersection, vector<int> trio) {
-    for (auto &match: intersection) {
-        if (!match->used) {
+bool addPeca(Board *b, int row, int col, vector<Piece *> intersection, vector<int> trio)
+{
+    for (auto &match : intersection)
+    {
+        if (!match->used)
+        {
 
-            if (match->num[0] == trio[2] && match->num[3] == trio[1] && match->num[2] == trio[0]) match->Rotate90();
+            if (match->num[0] == trio[2] && match->num[3] == trio[1] && match->num[2] == trio[0])
+                match->Rotate90();
 
-            if (match->num[3] == trio[2] && match->num[2] == trio[1] && match->num[1] == trio[0]) match->Rotate180();
+            if (match->num[3] == trio[2] && match->num[2] == trio[1] && match->num[1] == trio[0])
+                match->Rotate180();
 
-            if (match->num[2] == trio[2] && match->num[1] == trio[1] && match->num[0] == trio[0]) match->Rotate270();
+            if (match->num[2] == trio[2] && match->num[1] == trio[1] && match->num[0] == trio[0])
+                match->Rotate270();
 
             match->col = col;
             match->row = row;
@@ -343,24 +388,26 @@ bool addPeca(Board *b, int row, int col, vector<Piece *> intersection, vector<in
                 return true;
 
             // se estiver na ultima coluna passa para a linha de baixo
-            if (col == b->columns) {
+            if (col == b->columns)
+            {
                 col = 1;
                 row++;
-            } else
+            }
+            else
                 col++;
 
             if (tree(b, row, col))
                 return true;
 
-            //se estiver na ultima coluna volta para a linha de cima
-            if (col == 1) {
+            // se estiver na ultima coluna volta para a linha de cima
+            if (col == 1)
+            {
                 row--;
                 col = b->columns;
-            } else
+            }
+            else
                 col--;
 
-            match->row = match->col = 0;
-            match->used = false;
             int pieceIndex = find(b->board.begin(), b->board.end(), match) - b->board.begin();
             b->removeToALimit(pieceIndex);
         }
@@ -368,38 +415,30 @@ bool addPeca(Board *b, int row, int col, vector<Piece *> intersection, vector<in
     return false;
 }
 
-vector<Piece *> Intersection(vector<Piece *> &nums1, vector<Piece *> &nums2) {
+vector<Piece *> Intersection(vector<Piece *> &nums1, vector<int> trio)
+{
 
     sort(nums1.begin(), nums1.end()); // sort nums1 (non-decreasing order)
-    sort(nums2.begin(), nums2.end()); // sort nums2 (non-decreasing order)
 
-    int i = 0, j = 0;
-    vector < Piece * > res;
+    vector<Piece *> res;
 
-    while (i < (int) nums1.size() && j < (int) nums2.size()) {
-        if (i > 0 && nums1[i - 1] == nums1[i])
-            i++; // dealing with duplicates in nums1
-
-        else if (j > 0 && nums2[j - 1] == nums2[j])
-            j++; // dealing with duplicates in nums2
-
-        else if (nums1[i] < nums2[j])
-            i++; // if nums1 < nums2, we increment nums1
-
-        else if (nums1[i] > nums2[j])
-            j++; // if nums1 > nums2, we increment nums2
-
-        else {
-            res.push_back(nums1[i]); // if none of the above, nums1==nums2 so we add to result
-            i++;
-            j++;
+    for (auto &n : nums1)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (n->num[j] == trio[0] && n->num[(j + 1) % 4] == trio[1] && n->num[(j + 2) % 4] == trio[2])
+            {
+                res.push_back(n);
+                break;
+            }
         }
     }
 
     return res;
 }
 
-void reset(Board *b) {
+void reset(Board *b)
+{
     b->board.clear();
     b->pieces.clear();
     pairs.clear();
@@ -407,7 +446,8 @@ void reset(Board *b) {
     b->n_pieces = b->columns = b->rows = 0;
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
@@ -415,20 +455,23 @@ int main() {
 
     cin >> n;
 
-    for (int _ = 0; _ < n; _++) {
+    for (int _ = 0; _ < n; _++)
+    {
         int np = 0, r = 0, c = 0;
 
         cin >> np >> r >> c;
         Board *b = new Board(np, r, c);
 
-        for (int i = 0; i < np; i++) {
+        for (int i = 0; i < np; i++)
+        {
             int n0, n1, n2, n3 = 0;
             cin >> n0 >> n1 >> n2 >> n3;
             Piece *p = new Piece(n0, n1, n2, n3);
 
             getPairsAndOccurrencies(p);
 
-            if (i == 0) {
+            if (i == 0)
+            {
                 p->col = p->row = 1;
                 p->used = true;
                 b->board.push_back(p);
@@ -438,13 +481,15 @@ int main() {
         }
 
         int odd = 0;
-        for (auto &o: occurrences) {
+        for (auto &o : occurrences)
+        {
             if (o.second % 2 != 0)
                 odd++;
             if (odd > 4)
                 break;
         }
-        if (odd > 4) {
+        if (odd > 4)
+        {
             cout << "impossible puzzle!\n";
             reset(b);
             continue;
